4878fc7dc1e2baca427e5f1fb6c03a5a2853cac6
lumontexx <lumontesouza@gmail.com>
2022-05-19 23:56:07 -0300

    retirada de puntuacao

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function calcular() {
   const n2 = Number(altu.value);
   const imc = (n1 / (n2 * n2)).toFixed(2);
   if (imc <= 18.5) {
-    console.log((res.innerHTML = `Seu imc é ${imc}. Você está abaixo do peso`));
+    console.log((res.innerHTML = `Seu imc é ${imc} Você está abaixo do peso`));
   } else if(imc >= 18.5 && imc <= 24.9) {
-    console.log((res.innerHTML = `Seu imc é ${imc}. Seu peso é normal`));
+    console.log((res.innerHTML = `Seu imc é ${imc} Seu peso é normal`));
   } else if(imc >= 25.0 && imc <= 29.9) {
-    console.log((res.innerHTML = `Seu imc é ${imc}. Você está com sobrepeso`));
+    console.log((res.innerHTML = `Seu imc é ${imc} Você está com sobrepeso`));
   } else if(imc >= 30.0 && imc <= 34.9) {
-    console.log((res.innerHTML = `Seu imc é ${imc}. Você está com abesidade`));
+    console.log((res.innerHTML = `Seu imc é ${imc} Você está com abesidade`));
   }
 }
